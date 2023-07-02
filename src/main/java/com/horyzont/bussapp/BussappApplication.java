a408be3b191ebbe1d81c3808c45425eb9ad2ff4f
salimisler <dresizre@gmail.com>
2023-07-02 18:25:13 +0200

    init test

---
+++ src/main/java/com/horyzont/bussapp/BussappApplication.java
@@ -0,0 +1,13 @@
+package com.horyzont.bussapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BussappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BussappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/horyzont/bussapp/BussappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horyzont.bussapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BussappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
