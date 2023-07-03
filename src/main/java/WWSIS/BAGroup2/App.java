9067dce731f7b7e18fd73dfffb4bfdd01a03c21f
Ali Rıza Yücefaydalı <39056822+alirzaycefydli@users.noreply.github.com>
2023-07-03 16:47:51 +0200

    Added database configuration

--- src/main/java/WWSIS/BAGroup2/App.java
+++ src/main/java/WWSIS/BAGroup2/App.java
@@ -1,13 +1,38 @@
 package WWSIS.BAGroup2;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	static final String URL = "jdbc:hsqldb:hsql://localhost/";
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+        	Class.forName("org.hsqldb.jdbc.JDBCDriver");
+        	test();
+        }catch(Exception e) {
+        	throw new RuntimeException(e);
+        }
+    }
+    
+    private static void test() throws SQLException {
+    	Connection conn = DriverManager.getConnection(URL,"SA","");
+    	
+    	Statement stmt = conn.createStatement();
+    	ResultSet rs = stmt.executeQuery("SELECT * FROM messages;");
+    	
+    	
+    	while(rs.next()) {
+    		System.out.print("ID: "+ rs.getString("text") + "\n");
+    	}
     }
 }
