bc3f0c69fcd0603fe221a9c39e8e5cac74269aa7
Cem Arslan <cem.arslan@quadriga.eu>
2023-07-04 19:31:17 +0200

    subject entity added

--- src/main/java/com/horyzont/bussapp/entities/SubjectEntity.java
+++ src/main/java/com/horyzont/bussapp/entities/SubjectEntity.java
@@ -18,9 +18,65 @@ public class SubjectEntity {
     @JoinColumn(name = "teacher_id", referencedColumnName = "id")
     private TeacherEntity teacher_id;
     @Column(name = "start_date")
-    private Date start_date;
+    private String start_date;
     @Column(name = "end_date")
-    private Date end_date;
+    private String end_date;
     @Column(name = "grade_date")
-    private Date grade_date;
+    private String grade_date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TeacherEntity getTeacher_id() {
+        return teacher_id;
+    }
+
+    public String getStart_date() {
+        return start_date;
+    }
+
+    public String getEnd_date() {
+        return end_date;
+    }
+
+    public String getGrade_date() {
+        return grade_date;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTeacher_id(TeacherEntity teacher_id) {
+        this.teacher_id = teacher_id;
+    }
+
+    public void setStart_date(String start_date) {
+        this.start_date = start_date;
+    }
+
+    public void setEnd_date(String end_date) {
+        this.end_date = end_date;
+    }
+
+    public void setGrade_date(String grade_date) {
+        this.grade_date = grade_date;
+    }
 }
