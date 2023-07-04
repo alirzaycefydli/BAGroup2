2afa48defe7fbd659bf3de9e351391a768bd02ee
salimisler <dresizre@gmail.com>
2023-07-04 13:20:03 +0200

    sql file update with new id strategy

--- src/main/java/com/horyzont/bussapp/create-db.sql
+++ src/main/java/com/horyzont/bussapp/create-db.sql
@@ -3,7 +3,7 @@ drop schema public cascade;
 drop table students if exists;
 create table students
 (
-    id         INT NOT NULL PRIMARY KEY,
+    id         INTEGER IDENTITY PRIMARY KEY,
     first_name VARCHAR(255),
     last_name  VARCHAR(255),
     email      VARCHAR(255),
@@ -14,7 +14,7 @@ create table students
 drop table subjects if exists;
 create table subjects
 (
-    id          INT NOT NULL PRIMARY KEY,
+    id          INTEGER IDENTITY PRIMARY KEY,
     name        varchar(255),
     description varchar(255),
     teacher_id  INT,
@@ -26,7 +26,7 @@ create table subjects
 drop table teachers if exists;
 create table teachers
 (
-    id         INT NOT NULL PRIMARY KEY,
+    id         INTEGER IDENTITY PRIMARY KEY,
     first_name VARCHAR(255),
     last_name  VARCHAR(255),
     login      VARCHAR(255)
@@ -35,7 +35,7 @@ create table teachers
 drop table authors if exists;
 create table authors
 (
-    id         INT NOT NULL PRIMARY KEY,
+    id         INTEGER IDENTITY PRIMARY KEY,
     first_name VARCHAR(255),
     last_name  VARCHAR(255)
 );
@@ -43,7 +43,7 @@ create table authors
 drop table books if exists;
 create table books
 (
-    id             INT NOT NULL PRIMARY KEY,
+    id             INTEGER IDENTITY PRIMARY KEY,
     title          VARCHAR(255),
     author         INT,
     published_date DATE,
@@ -53,52 +53,52 @@ create table books
 drop table borrows if exists;
 create table borrows
 (
-    id          INT NOT NULL PRIMARY KEY,
+    id          INTEGER IDENTITY PRIMARY KEY,
     student_id  INT,
     book_id     INT,
-    borrow_date DATE,
-    return_date DATE,
+    borrow_date VARCHAR(255),
+    return_date VARCHAR(255),
     status      BIT,
     FOREIGN KEY (student_id) REFERENCES students (id),
     FOREIGN KEY (book_id) REFERENCES books (id)
 );
 
-INSERT INTO students (id, first_name, last_name, email, password, login)
-VALUES (1, 'Cem', 'Arslan', 'cemarslan@gmail.com', '123123', 'cem.arslan');
-INSERT INTO students (id, first_name, last_name, email, password, login)
-VALUES (2, 'Ali Riza', 'Yucefaydali', 'aliriza@gmail.com', 'abcabc', 'ali.riza');
+INSERT INTO students (first_name, last_name, email, password, login)
+VALUES ('Cem', 'Arslan', 'cemarslan@gmail.com', '123123', 'cem.arslan');
+INSERT INTO students (first_name, last_name, email, password, login)
+VALUES ('Ali Riza', 'Yucefaydali', 'aliriza@gmail.com', 'abcabc', 'ali.riza');
 
-INSERT INTO teachers (id, first_name, last_name, login)
-values (1, 'Cezar', 'Holub', 'cezar.holub');
-INSERT INTO teachers (id, first_name, last_name, login)
-values (2, 'Pawel', 'Moriewic', 'pawel.moriewic');
+INSERT INTO teachers (first_name, last_name, login)
+values ('Cezar', 'Holub', 'cezar.holub');
+INSERT INTO teachers (first_name, last_name, login)
+values ('Pawel', 'Moriewic', 'pawel.moriewic');
 
-INSERT INTO subjects (id, name, description, teacher_id, start_date, end_date, grade_date)
-VALUES (1, 'Math', 'Math Subject Description', 1, '2016-01-02', '2017-01-02', '2017-01-02');
+INSERT INTO subjects (name, description, teacher_id, start_date, end_date, grade_date)
+VALUES ('Math', 'Math Subject Description', 1, '2016-01-02', '2017-01-02', '2017-01-02');
 
-INSERT INTO authors (id, first_name, last_name)
-VALUES (1, 'Salim', 'Isler');
-INSERT INTO authors (id, first_name, last_name)
-VALUES (2, 'Kadir', 'Kocak');
+INSERT INTO authors (first_name, last_name)
+VALUES ('Salim', 'Isler');
+INSERT INTO authors (first_name, last_name)
+VALUES ('Kadir', 'Kocak');
 
-INSERT INTO books (id, title, author, published_date)
-VALUES (1, 'Book 1', 1, '1997-01-02');
-INSERT INTO books (id, title, author, published_date)
-VALUES (2, 'Book 2', 1, '1991-01-02');
-INSERT INTO books (id, title, author, published_date)
-VALUES (3, 'Book 3', 1, '1992-01-02');
-INSERT INTO books (id, title, author, published_date)
-VALUES (4, 'Book 4', 1, '1993-01-02');
-INSERT INTO books (id, title, author, published_date)
-VALUES (5, 'Book 5', 1, '1994-01-02');
+INSERT INTO books (title, author, published_date)
+VALUES ('Book 1', 0, '1997-01-02');
+INSERT INTO books (title, author, published_date)
+VALUES ('Book 2', 0, '1991-01-02');
+INSERT INTO books (title, author, published_date)
+VALUES ('Book 3', 0, '1992-01-02');
+INSERT INTO books (title, author, published_date)
+VALUES ('Book 4', 1, '1993-01-02');
+INSERT INTO books (title, author, published_date)
+VALUES ('Book 5', 1, '1994-01-02');
 
-INSERT INTO borrows (id, student_id, book_id, borrow_date, return_date, status)
-VALUES (1, 1, 1, '2016-01-02', '2016-02-02', 0);
-INSERT INTO borrows (id, student_id, book_id, borrow_date, return_date, status)
-VALUES (2, 1, 2, '2016-01-02', '2016-02-02', 0);
-INSERT INTO borrows (id, student_id, book_id, borrow_date, return_date, status)
-VALUES (3, 2, 3, '2016-01-02', '2016-02-02', 0);
-INSERT INTO borrows (id, student_id, book_id, borrow_date, return_date, status)
-VALUES (4, 2, 4, '2016-01-02', '2016-02-02', 0);
-INSERT INTO borrows (id, student_id, book_id, borrow_date, return_date, status)
-VALUES (5, 2, 5, '2016-01-02', '2016-02-02', 0);
+INSERT INTO borrows (student_id, book_id, borrow_date, return_date, status)
+VALUES (0, 0, '2016-01-02', '2016-02-02', 0);
+INSERT INTO borrows (student_id, book_id, borrow_date, return_date, status)
+VALUES (0, 1, '2016-01-02', '2016-02-02', 0);
+INSERT INTO borrows (student_id, book_id, borrow_date, return_date, status)
+VALUES (1, 2, '2016-01-02', '2016-02-02', 0);
+INSERT INTO borrows (student_id, book_id, borrow_date, return_date, status)
+VALUES (1, 3, '2016-01-02', '2016-02-02', 0);
+INSERT INTO borrows (student_id, book_id, borrow_date, return_date, status)
+VALUES (1, 4, '2016-01-02', '2016-02-02', 0);
