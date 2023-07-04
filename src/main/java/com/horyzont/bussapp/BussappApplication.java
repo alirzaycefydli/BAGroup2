14c21db4ec1a59ab4a79014192be381f4dd8a059
salimisler <dresizre@gmail.com>
2023-07-04 13:22:33 +0200

    merge error fixed

--- src/main/java/com/horyzont/bussapp/BussappApplication.java
+++ src/main/java/com/horyzont/bussapp/BussappApplication.java
@@ -1,21 +1,13 @@
 package com.horyzont.bussapp;
 
-import com.horyzont.bussapp.dao.AuthorDaoImpl;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class BussappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BussappApplication.class, args);
-
-        AuthorDaoImpl imp = new AuthorDaoImpl();
-
-        System.out.print(imp.getById(1));
     }
 
 }
