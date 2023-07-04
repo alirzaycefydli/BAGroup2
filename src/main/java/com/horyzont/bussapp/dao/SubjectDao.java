83d5a2dafd686f4310f46f446adf5463b54f0a55
Cem Arslan <cem.arslan@quadriga.eu>
2023-07-04 19:33:25 +0200

    added dao

--- src/main/java/com/horyzont/bussapp/dao/SubjectDao.java
+++ src/main/java/com/horyzont/bussapp/dao/SubjectDao.java
@@ -5,6 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SubjectDao extends JpaRepository<SubjectEntity, Integer> {
+    @Query("SELECT s from SubjectEntity s WHERE s.teacher_id.id = ?1")
+    List<SubjectEntity> findAllByTeacherId(Integer teacher_id);
 }
