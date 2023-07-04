a213d390c18103431c5af30d8abcd9cd02ae48a9
Kadir94 <kadirkocak94@yandex.com>
2023-07-04 18:48:55 +0200

    queries added for dao

--- src/main/java/com/horyzont/bussapp/dao/TeacherDao.java
+++ src/main/java/com/horyzont/bussapp/dao/TeacherDao.java
@@ -7,5 +7,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeacherDao extends JpaRepository<TeacherEntity, Integer> {
-    
+    @Query("SELECT count(1) from TeacherEntity s WHERE s.email = ?1 AND s.password = ?2")
+    public Integer findByMailAndPassword(String email , String password);
+
+    @Query("SELECT s FROM TeacherEntity s WHERE s.email = ?1")
+    public TeacherEntity findByMail(String email);
 }
