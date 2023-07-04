72c2964c0172cb7921930c5a8025bbadab98fc21
Cem Arslan <cem.arslan@quadriga.eu>
2023-07-04 19:28:19 +0200

    subject service added

--- src/main/java/com/horyzont/bussapp/service/SubjectService.java
+++ src/main/java/com/horyzont/bussapp/service/SubjectService.java
@@ -1,11 +1,12 @@
 package com.horyzont.bussapp.service;
 
-import com.horyzont.bussapp.dao.StudentDao;
 import com.horyzont.bussapp.dao.SubjectDao;
-import com.horyzont.bussapp.entities.StudentEntity;
 import com.horyzont.bussapp.entities.SubjectEntity;
+import com.horyzont.bussapp.entities.TeacherEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SubjectService {
@@ -13,9 +14,32 @@ public class SubjectService {
     @Autowired
     SubjectDao subjectDao;
 
-    public void subscribe(Integer id){
-        SubjectEntity entity = new SubjectService();
-        entity.setId(id);
-        subjectDao.delete(entity);
+    public SubjectEntity addSubject(String name, String desc, Integer teacher_id, String start_date, String end_date, String grade_date) {
+        TeacherEntity teacherEntity = new TeacherEntity();
+        teacherEntity.setId(teacher_id);
+
+        SubjectEntity subjectEntity = new SubjectEntity();
+        subjectEntity.setName(name);
+        subjectEntity.setDescription(desc);
+        subjectEntity.setTeacher_id(teacherEntity);
+        subjectEntity.setStart_date(start_date);
+        subjectEntity.setEnd_date(end_date);
+        subjectEntity.setGrade_date(grade_date);
+        subjectDao.save(subjectEntity);
+        return subjectEntity;
+    }
+
+    public void removeSubject(Integer subject_id) {
+        SubjectEntity subjectEntity = new SubjectEntity();
+        subjectEntity.setId(subject_id);
+        subjectDao.delete(subjectEntity);
+    }
+
+    public List<SubjectEntity> getAllSubject() {
+        return subjectDao.findAll();
+    }
+
+    public List<SubjectEntity> getAllSubjectByTeacherId(Integer student_id) {
+        return subjectDao.findAllByTeacherId(student_id);
     }
 }
